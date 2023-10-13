ea66175daae87aeee8f6ee59eb2dfa924223a9ff
Bwise1 <oguntoyebenjamin2@gmail.com>
2023-10-13 07:01:31 +0300

    modified dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
-import type { Config } from "tailwindcss";
+// import type { Config } from "tailwindcss";
 const plugin = require("tailwindcss/plugin");
 
-const config: Config = {
+const config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
